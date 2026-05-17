910260356ac08ac1b6190e08fc3d3f6bad9295ae
Prathamesh Zagade <prathamzagade1811@gmail.com>
2026-05-17 12:47:55 +0530

    flask files

--- Python Solving/Flask/main.py
+++ Python Solving/Flask/main.py
@@ -4,13 +4,13 @@ which will be your WSGI (Web Server Gateway Interface) applicaation'''
 
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return "Welcome to this flask course. This should be an amazing course. This is the best course and in this course you will learn about the flask framework"
-
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
