6e6cd1f1335392da7aaf556876ab76fc0f8dbd6e
Prathamesh Zagade <prathamzagade1811@gmail.com>
2026-05-18 14:56:07 +0530

    getpost

--- Python Solving/Flask/getpost.py
+++ Python Solving/Flask/getpost.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template, request
-'''It creates an instance of a flask class, 
-which will be your WSGI (Web Server Gateway Interface) applicaation'''
 
 app = Flask(__name__)
 
-@app.route('/index',methods=['GET'])
-def index():
+@app.route('/')
+def home():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/form',methods = ['GET','POST'] )
+
+@app.route('/form', methods=['GET', 'POST'])
 def form():
-    if request.method=='POST':
-        pass
-        return render_template('form.html')
+
+    if request.method == 'POST':
+        name = request.form['name']
+        return f"Hello, {name}"
+
+    return render_template('form.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
