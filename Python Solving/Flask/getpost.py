09940fb28e3bad6b3fed1cf63631aec67cdeb4f1
Prathamesh Zagade <prathamzagade1811@gmail.com>
2026-05-18 13:53:05 +0530

    push

---
+++ Python Solving/Flask/getpost.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+'''It creates an instance of a flask class, 
+which will be your WSGI (Web Server Gateway Interface) applicaation'''
+
+app = Flask(__name__)
+
+@app.route('/index',methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/form',methods = ['GET','POST'] )
+def form():
+    if request.method=='POST':
+        pass
+        return render_template('form.html')
+if __name__ == '__main__':
+    app.run(debug=True)
