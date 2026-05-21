74f36a390d65e4dcf0e17e12341c16a646fb2389
Prathamesh Zagade <prathamzagade1811@gmail.com>
2026-05-21 23:59:59 +0530

    Streamlit update

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Streamlit Text Input")
 
@@ -16,6 +17,25 @@ if age==0:
     st.write(f"Hey {name} Sorry you are not eligible")
 elif age <=10:
     st.write(f"Hey {name}! Sorry you are not eligible")
-    
+
 else :
     st.write("Hurray! You are eligible")
+
+data = {
+
+    "Name": ["Alice", "Bob", "Charlie", "David"],
+
+    "Age": [22, 25, 21, 24],
+
+    "City": ["New York", "London", "Paris", "Tokyo"]
+
+}
+
+df = pd.DataFrame(data)
+st.write(df)
+
+uploaded_file = st.file_uploader("Choose a csv file")
+
+if uploaded_file is not None:
+    df=pd.read_csv(uploaded_file)
+    st.write(df)
