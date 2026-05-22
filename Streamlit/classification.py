8307d5cb9a8c96e95566d558361176b8397c0d37
Prathamesh Zagade <prathamzagade1811@gmail.com>
2026-05-22 13:27:03 +0530

    push

--- Streamlit/classification.py
+++ Streamlit/classification.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
-@st.cache
+@st.cache_data
 def load_data():
     iris = load_iris()
     df = pd.DataFrame(iris.data, columns=iris.feature_names)
